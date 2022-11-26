1e4f21fc97f18190cf32a43dbcf5f93c6444d120
Dajesa <93830906+Dajesa@users.noreply.github.com>
2022-11-26 12:21:32 -0500

    primer commit envio de los archivos

---
+++ src/main/java/com/dajesaSoft0937/proyectoudigitalprogramacionWeb01/ProyectoudigitalProgramacionWeb01Application.java
@@ -0,0 +1,13 @@
+package com.dajesaSoft0937.proyectoudigitalprogramacionWeb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoudigitalProgramacionWeb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoudigitalProgramacionWeb01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dajesaSoft0937/proyectoudigitalprogramacionWeb01/ProyectoudigitalProgramacionWeb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dajesaSoft0937.proyectoudigitalprogramacionWeb01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoudigitalProgramacionWeb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
